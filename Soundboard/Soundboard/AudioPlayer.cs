08f4d2f1e7b254865cf27f93bc0863e110918d49
Alex Novak <alexwnovak@gmail.com>
2017-08-29 20:14:55 -0400

    No longer fails to play once you play after it's stopped
    Really simplified things here--didn't need the stateful boolean at all. Just stop and start--every time. Works great.

--- Soundboard/Soundboard/AudioPlayer.cs
+++ Soundboard/Soundboard/AudioPlayer.cs
@@ -6,27 +6,17 @@ namespace Soundboard
    public class AudioPlayer : IAudioPlayer
    {
       private MediaPlayer _mediaPlayer;
-      private bool _isPlaying;
 
       public void Initialize()
       {
          _mediaPlayer = new MediaPlayer();
          _mediaPlayer.Open( new Uri( "Resources/Airhorn.wav", UriKind.Relative ) );
-         _mediaPlayer.MediaEnded += ( _, __ ) => _isPlaying = false;
       }
 
       public void Play()
       {
-         if ( _isPlaying )
-         {
-            _mediaPlayer.Stop();
-            _mediaPlayer.Play();
-         }
-         else
-         {
-            _mediaPlayer.Play();
-            _isPlaying = true;
-         }
+         _mediaPlayer.Stop();
+         _mediaPlayer.Play();
       }
    }
 }
