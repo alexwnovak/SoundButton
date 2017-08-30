8110faefe93f1e470f558b10967ce9afcd0f32d2
Alex Novak <alexwnovak@gmail.com>
2017-08-29 22:30:34 -0400

    Long-pressing now fades in the menu items dynamically
    It rips through each thing in the stack panel and assigns it a name and an animation, composing it together into a storboard. It staggers when each starts--later elements start later for a neat cascading effect.
    
    Can we cache this somehow to make it smarter?

--- Soundboard/Soundboard/Behaviors/MouseInteractionBehavior.cs
+++ Soundboard/Soundboard/Behaviors/MouseInteractionBehavior.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interactivity;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using Soundboard.ViewModels;
+using Soundboard.Views;
 
 namespace Soundboard.Behaviors
 {
-   public class MouseInteractionBehavior : Behavior<Window>
+   public class MouseInteractionBehavior : Behavior<MainWindow>
    {
       private readonly InteractionInterpreter _interactionInterpreter = new InteractionInterpreter();
+      private readonly List<string> _nameList = new List<string>();
       private MainViewModel _mainViewModel;
 
       public MouseInteractionBehavior()
@@ -55,7 +60,46 @@ namespace Soundboard.Behaviors
 
       private void OnLongPress()
       {
+         var storyboard = new Storyboard();
+         int beginTime = 0;
+
+         NameScope.SetNameScope( this, new NameScope() );
+
+         foreach ( UIElement item in AssociatedObject.MenuStackPanel.Children )
+         {
+            var fadeAnimation = new DoubleAnimation( 0, 1, new Duration( TimeSpan.FromSeconds( 0.4 ) ) );
+            Storyboard.SetTarget( fadeAnimation, item );
+            Storyboard.SetTargetProperty( fadeAnimation, new PropertyPath( UIElement.OpacityProperty ) );
+            storyboard.Children.Add( fadeAnimation );
+
+            string transformName = $"TranslateTransform{beginTime}";
+
+            if ( !_nameList.Contains( transformName ) )
+            {
+               _nameList.Add( transformName );
+               item.RenderTransform = new TranslateTransform( -20, 0 );
+               AssociatedObject.RegisterName( transformName, item.RenderTransform );
+            }
+
+            var moveAnimation = new DoubleAnimation( -20, 0, new Duration( TimeSpan.FromSeconds( 0.4 ) ) )
+            {
+               BeginTime = TimeSpan.FromMilliseconds( beginTime ),
+               EasingFunction = new CircleEase
+               {
+                  EasingMode = EasingMode.EaseOut
+               }
+            };
+
+            Storyboard.SetTargetName( moveAnimation, transformName );
+            Storyboard.SetTargetProperty( moveAnimation, new PropertyPath( TranslateTransform.XProperty ) );
+            storyboard.Children.Add( moveAnimation );
+
+            beginTime += 120;
+         }
+
          AssociatedObject.Width = 500;
+
+         storyboard.Begin( AssociatedObject );
       }
    }
 }
