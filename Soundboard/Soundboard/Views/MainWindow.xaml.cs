ef332114abf970fefea72a33a1fb06c36e45c91b
Alex Novak <alexwnovak@gmail.com>
2017-08-29 21:47:34 -0400

    Hides the menu items after choosing one or changing apps

--- Soundboard/Soundboard/Views/MainWindow.xaml.cs
+++ Soundboard/Soundboard/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Soundboard.ViewModels;
 
 namespace Soundboard.Views
@@ -10,8 +11,23 @@ namespace Soundboard.Views
          InitializeComponent();
 
          var viewModel = (MainViewModel) DataContext;
-         viewModel.MinimizeReqested += ( _, __ ) => WindowState = WindowState.Minimized;
-         viewModel.ExitRequested += ( _, __ ) => Close();
+         viewModel.MinimizeReqested += ( _, __ ) => OnMinimizeRequested();
+         viewModel.ExitRequested += ( _, __ ) => OnExitRequested();
       }
+
+      private void ResetWindowWidth() => Width = 160;
+
+      private void OnMinimizeRequested()
+      {
+         ResetWindowWidth();
+         WindowState = WindowState.Minimized;
+      }
+
+      private void OnExitRequested()
+      {
+         Close();
+      }
+
+      private void MainWindow_OnDeactivated( object sender, EventArgs e ) => ResetWindowWidth();
    }
 }
