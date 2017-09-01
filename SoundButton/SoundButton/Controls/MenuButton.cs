66e935f20848870582c2fa648cdc9341ada47052
Alex Novak <alexwnovak@gmail.com>
2017-08-31 22:53:24 -0400

    MenuButton now does a MouseOver visual state

--- SoundButton/SoundButton/Controls/MenuButton.cs
+++ SoundButton/SoundButton/Controls/MenuButton.cs
@@ -1,10 +1,33 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SoundButton.Controls
 {
    public class MenuButton : ContentControl
    {
+      private Border _outerBorder;
+      public Border OuterBorder
+      {
+         get => _outerBorder;
+         set
+         {
+            if ( _outerBorder != null )
+            {
+               _outerBorder.MouseEnter -= OuterBorderMouseEnter;
+               _outerBorder.MouseLeave -= OuterBorderMouseLeave;
+            }
+
+            _outerBorder = value;
+
+            if ( _outerBorder != null )
+            {
+               _outerBorder.MouseEnter += OuterBorderMouseEnter;
+               _outerBorder.MouseLeave += OuterBorderMouseLeave;
+            }
+         }
+      }
+
       public static DependencyProperty CornerRadiusProperty = DependencyProperty.Register( nameof( CornerRadius ),
          typeof( CornerRadius ),
          typeof( MenuButton ),
@@ -19,6 +42,21 @@ namespace SoundButton.Controls
       static MenuButton()
       {
          DefaultStyleKeyProperty.OverrideMetadata( typeof( MenuButton ), new FrameworkPropertyMetadata( typeof( MenuButton ) ) );
+      }
+
+      public override void OnApplyTemplate()
+      {
+         OuterBorder = GetTemplateChild( "OuterBorder" ) as Border;
+      }
+
+      private void OuterBorderMouseEnter( object sender, MouseEventArgs e )
+      {
+         VisualStateManager.GoToState( this, "MouseOver", true );
+      }
+
+      private void OuterBorderMouseLeave( object sender, MouseEventArgs e )
+      {
+         VisualStateManager.GoToState( this, "Normal", true );
       }
    }
 }
