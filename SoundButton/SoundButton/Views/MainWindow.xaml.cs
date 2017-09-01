ca60e267f2ff519e7a8eb2b669a32b9c65a38ae2
Alex Novak <alexwnovak@gmail.com>
2017-09-01 13:52:35 -0400

    Wiring a left click event to the PlayCommand
    Looks dumb to chain them together manually. Will shore this up in the future. Laying the groundwork for now.

--- SoundButton/SoundButton/Views/MainWindow.xaml.cs
+++ SoundButton/SoundButton/Views/MainWindow.xaml.cs
@@ -7,14 +7,15 @@ namespace SoundButton.Views
    public partial class MainWindow : Window
    {
       private double _originalWidth;
+      private readonly MainViewModel _viewModel;
 
       public MainWindow()
       {
          InitializeComponent();
 
-         var viewModel = (MainViewModel) DataContext;
-         viewModel.MinimizeReqested += ( _, __ ) => OnMinimizeRequested();
-         viewModel.ExitRequested += ( _, __ ) => OnExitRequested();
+         _viewModel = (MainViewModel) DataContext;
+         _viewModel.MinimizeReqested += ( _, __ ) => OnMinimizeRequested();
+         _viewModel.ExitRequested += ( _, __ ) => OnExitRequested();
       }
 
       private void ResetWindowWidth() => Width = _originalWidth;
@@ -32,5 +33,10 @@ namespace SoundButton.Views
 
       private void MainWindow_OnLoaded( object sender, RoutedEventArgs e ) => _originalWidth = Width;
       private void MainWindow_OnDeactivated( object sender, EventArgs e ) => ResetWindowWidth();
+
+      private void MenuButton_OnLeftClick( object sender, RoutedEventArgs e )
+      {
+         _viewModel.PlayCommand.Execute( null );
+      }
    }
 }
