a9806cc1f6476d5b924ef817885840f6fd741da9
Alex Novak <alexwnovak@gmail.com>
2017-08-31 21:39:29 -0400

    +Basic MenuButton derivative of ContentControl
    We'll do a style later to give it visual structure.

---
+++ SoundButton/SoundButton/Controls/MenuButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SoundButton.Controls
+{
+   public class MenuButton : ContentControl
+   {
+      public static DependencyProperty CornerRadiusProperty = DependencyProperty.Register( nameof( CornerRadius ),
+         typeof( CornerRadius ),
+         typeof( MenuButton ),
+         new PropertyMetadata( new CornerRadius( 0 ) ) );
+
+      public CornerRadius CornerRadius
+      {
+         get => (CornerRadius) GetValue( CornerRadiusProperty );
+         set => SetValue( CornerRadiusProperty, value );
+      }
+   }
+}
