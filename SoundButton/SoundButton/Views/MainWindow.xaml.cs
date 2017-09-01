d6e6996045f6f1ab431094ebe01272b78d22f837
Alex Novak <alexwnovak@gmail.com>
2017-09-01 13:57:07 -0400

    Right-clicking now opens the menu

--- SoundButton/SoundButton/Views/MainWindow.xaml.cs
+++ SoundButton/SoundButton/Views/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using SoundButton.ViewModels;
 
 namespace SoundButton.Views
@@ -8,6 +11,7 @@ namespace SoundButton.Views
    {
       private double _originalWidth;
       private readonly MainViewModel _viewModel;
+      private readonly List<string> _nameList = new List<string>();
 
       public MainWindow()
       {
@@ -37,6 +41,53 @@ namespace SoundButton.Views
       private void MenuButton_OnLeftClick( object sender, RoutedEventArgs e )
       {
          _viewModel.PlayCommand.Execute( null );
+      }
+
+      private void MenuButton_OnRightClick( object sender, RoutedEventArgs e )
+      {
+         FadeMenuIn();
+      }
+
+      private void FadeMenuIn()
+      {
+         var storyboard = new Storyboard();
+         int beginTime = 0;
+
+         foreach ( UIElement item in MenuStackPanel.Children )
+         {
+            var fadeAnimation = new DoubleAnimation( 0, 1, new Duration( TimeSpan.FromSeconds( 0.4 ) ) );
+            Storyboard.SetTarget( fadeAnimation, item );
+            Storyboard.SetTargetProperty( fadeAnimation, new PropertyPath( OpacityProperty ) );
+            storyboard.Children.Add( fadeAnimation );
+
+            string transformName = $"TranslateTransform{beginTime}";
+
+            if ( !_nameList.Contains( transformName ) )
+            {
+               _nameList.Add( transformName );
+               item.RenderTransform = new TranslateTransform( -20, 0 );
+               RegisterName( transformName, item.RenderTransform );
+            }
+
+            var moveAnimation = new DoubleAnimation( -20, 0, new Duration( TimeSpan.FromSeconds( 0.4 ) ) )
+            {
+               BeginTime = TimeSpan.FromMilliseconds( beginTime ),
+               EasingFunction = new CircleEase
+               {
+                  EasingMode = EasingMode.EaseOut
+               }
+            };
+
+            Storyboard.SetTargetName( moveAnimation, transformName );
+            Storyboard.SetTargetProperty( moveAnimation, new PropertyPath( TranslateTransform.XProperty ) );
+            storyboard.Children.Add( moveAnimation );
+
+            beginTime += 120;
+         }
+
+         Width = 500;
+
+         storyboard.Begin( this );
       }
    }
 }
