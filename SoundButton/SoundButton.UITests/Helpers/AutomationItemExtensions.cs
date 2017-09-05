179c734fbab2c1a90f2c38828928ca1b6e6d7291
Alex Novak <alexwnovak@gmail.com>
2017-09-05 19:27:48 -0400

    +Extensions class that currently has a Find method

---
+++ SoundButton/SoundButton.UITests/Helpers/AutomationItemExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Automation;
+
+namespace SoundButton.UITests.Helpers
+{
+   public static class AutomationItemExtensions
+   {
+      public static AutomationItem Find( this AutomationItem automationItem, Property by, object value )
+      {
+         var condition = by.GetCondition( value );
+         var childElement = automationItem.AutomationElement.FindFirst( TreeScope.Children, condition );
+         return new AutomationItem( childElement );
+      }
+   }
+}
