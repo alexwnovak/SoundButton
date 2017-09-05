4a06ec2c07c4a6c1c8ed43648782c2b1ab5cde0d
Alex Novak <alexwnovak@gmail.com>
2017-09-05 19:27:06 -0400

    +Property class that houses properties you can search by
    Also serves as an abstraction to the automation API.

---
+++ SoundButton/SoundButton.UITests/Helpers/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Automation;
+
+namespace SoundButton.UITests.Helpers
+{
+   public class Property
+   {
+      private readonly Func<object, PropertyCondition> _propertyConditionCreator;
+
+      public static Property ProcessId { get; } = new Property( obj => new PropertyCondition( AutomationElement.ProcessIdProperty, obj ) );
+      public static Property AutomationId { get; } = new Property( obj => new PropertyCondition( AutomationElement.AutomationIdProperty, obj ) );
+
+      private Property( Func<object, PropertyCondition> propertyConditionCreator )
+      {
+         _propertyConditionCreator = propertyConditionCreator;
+      }
+
+      internal PropertyCondition GetCondition( object obj ) => _propertyConditionCreator( obj );
+   }
+}
