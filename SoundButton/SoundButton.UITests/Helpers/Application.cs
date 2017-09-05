2933be6c82a9fbe61ca3af8569ed5443f9116942
Alex Novak <alexwnovak@gmail.com>
2017-09-05 18:26:23 -0400

    +Application class that wraps a process
    This will be the thing that you call first to launch your application. It'll be the conduit to getting the main window, and then query things in the window.

---
+++ SoundButton/SoundButton.UITests/Helpers/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SoundButton.UITests.Helpers
+{
+   public class Application : IDisposable
+   {
+      private readonly Process _applicationProcess;
+
+      private Application( Process applicationProcess )
+      {
+         _applicationProcess = applicationProcess;
+      }
+
+      public static Application Launch( string applicationPath )
+      {
+         var applicationProcess = Process.Start( applicationPath );
+
+         return new Application( applicationProcess );
+      }
+
+      public void Dispose()
+      {
+         _applicationProcess.Kill();
+         _applicationProcess.Dispose();
+      }
+   }
+}
