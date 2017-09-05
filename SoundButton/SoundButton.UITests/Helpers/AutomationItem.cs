4902534a3d6f16d95088c3514ffdb531ab15a394
Alex Novak <alexwnovak@gmail.com>
2017-09-05 18:39:13 -0400

    +AutomationItem, an abstraction over AutomationElement
    This can hold interaction and searching abilities.

---
+++ SoundButton/SoundButton.UITests/Helpers/AutomationItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Automation;
+
+namespace SoundButton.UITests.Helpers
+{
+   public class AutomationItem
+   {
+      internal AutomationElement AutomationElement
+      {
+         get;
+         set;
+      }
+
+      internal AutomationItem()
+      {
+      }
+   }
+}
