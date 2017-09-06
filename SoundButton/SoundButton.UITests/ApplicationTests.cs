c813aaff74854f680191824694edf09338ae86e1
Alex Novak <alexwnovak@gmail.com>
2017-09-06 15:47:18 -0400

    +Test to long-press the button and then click Exit

--- SoundButton/SoundButton.UITests/ApplicationTests.cs
+++ SoundButton/SoundButton.UITests/ApplicationTests.cs
@@ -1,6 +1,31 @@
+﻿using System;
+using Xunit;
+using SoundButton.UITests.Helpers;
+
 namespace SoundButton.UITests
 {
-   public class ApplicationTests
+   public class ApplicationTests : IDisposable
    {
+      private readonly Application _application;
+
+      public ApplicationTests()
+      {
+         _application = Application.Launch( @"..\..\..\SoundButton\bin\Debug\SoundButton.exe" );
+      }
+
+      public void Dispose()
+      {
+         _application.Dispose();
+      }
+
+      [Fact]
+      public void AppLaunches_ExitButtonIsClicked_AppExits()
+      {
+         var playButton = _application.MainWindow.Find( Property.AutomationId, "ai-PlayButton" );
+         playButton.Press( TimeSpan.FromSeconds( 1 ) );
+
+         var exitButton = _application.MainWindow.Find( Property.AutomationId, "ai-ExitButton" );
+         exitButton.Click();
+      }
    }
 }
