7421cd6070d6efd79e590239c01d0686efda2066
Alex Novak <alexwnovak@gmail.com>
2017-09-06 15:17:30 -0400

    Exposing a way to find from AutomationElement, not just AutomationItem

--- SoundButton/SoundButton.UITests/Helpers/AutomationItemExtensions.cs
+++ SoundButton/SoundButton.UITests/Helpers/AutomationItemExtensions.cs
@@ -1,14 +1,37 @@
-﻿using System.Windows.Automation;
+﻿using System;
+using System.Threading;
+using System.Windows.Automation;
 
 namespace SoundButton.UITests.Helpers
 {
    public static class AutomationItemExtensions
    {
+      public static AutomationElement Find( this AutomationElement automationElement, Property by, object value )
+      {
+         var propertyCondition = by.GetCondition( value );
+
+         for ( int attempt = 0; attempt < 10; attempt++ )
+         {
+            System.Diagnostics.Debug.WriteLine( "Looking for element" );
+
+            var childElement = automationElement.FindFirst( TreeScope.Children, propertyCondition );
+
+            if ( childElement != null )
+            {
+               System.Diagnostics.Debug.WriteLine( "Element found" );
+               return childElement;
+            }
+
+            System.Diagnostics.Debug.WriteLine( "Element not found, sleeping..." );
+            Thread.Sleep( 100 );
+         }
+
+         throw new Exception();
+      }
+
       public static AutomationItem Find( this AutomationItem automationItem, Property by, object value )
       {
-         var condition = by.GetCondition( value );
-         var childElement = automationItem.AutomationElement.FindFirst( TreeScope.Children, condition );
-         return new AutomationItem( childElement );
+         return new AutomationItem( Find( automationItem.AutomationElement, by, value ) );
       }
    }
 }
