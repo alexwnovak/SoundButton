24e8e723fddff45d0a159d85451ba425f2860525
Alex Novak <alexwnovak@gmail.com>
2017-09-16 17:31:20 -0400

    +IVisualStateManager abstraction that wraps the real thing

---
+++ SoundButton/SoundButton/Controls/IVisualStateManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace SoundButton.Controls
+{
+   public interface IVisualStateManager
+   {
+      bool GoToState( FrameworkElement control, string stateName, bool useTransitions );
+   }
+}
---
+++ SoundButton/SoundButton/Controls/VisualStateManagerAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SoundButton.Controls
+{
+   public class VisualStateManagerAdapter : IVisualStateManager
+   {
+      public bool GoToState( FrameworkElement control, string stateName, bool useTransitions )
+      {
+         return VisualStateManager.GoToState( control, stateName, useTransitions );
+      }
+   }
+}
