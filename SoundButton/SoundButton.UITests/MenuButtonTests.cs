8b9706e5ec7e06739c9a5043caaebb6fa243c3ed
Alex Novak <alexwnovak@gmail.com>
2017-09-17 20:26:44 -0400

    +Test for mousing over a MenuButton should enter the right visual state

---
+++ SoundButton/SoundButton.UITests/MenuButtonTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Automation;
+using Xunit;
+using FlaUI.Core.Input;
+using Moq;
+using Lodestone.Wpf;
+using SoundButton.Controls;
+using SoundButton.UITests.Helpers;
+
+namespace SoundButton.UITests
+{
+   public class MenuButtonTests
+   {
+      [Fact]
+      public void HoversMouseOverControl_EntersMouseOverVisualState()
+      {
+         var visualStateManagerMock = new Mock<IVisualStateManager>();
+
+         var controlBuilder = ControlBuilder.For( () => new MenuButton( visualStateManagerMock.Object ) )
+            .With( b => AutomationProperties.SetAutomationId( b, "ai-Button" ) );
+
+         var controlHost = ControlHost<MenuButton>.Launch( controlBuilder );
+
+         int processId = Process.GetCurrentProcess().Id;
+         var mainWindow = AutomationElement.RootElement.Find( Property.ProcessId, processId );
+         var button = mainWindow.Find( Property.AutomationId, "ai-Button" );
+
+         var rect = (Rect) button.GetCurrentPropertyValue( AutomationElement.BoundingRectangleProperty );
+         Mouse.MoveTo( (int) rect.Left, (int) rect.Top );
+
+         visualStateManagerMock.Verify( vsm => vsm.GoToState( controlHost.Control, "MouseOver", true ), Times.Once() );
+
+         controlHost.Close();
+      }
+   }
+}
